f3b875618f5657a9f16fe59c316505f03d2bb412
Vaibhav Saini <vaibps17@gmail.com>
2016-04-08 22:18:24 -0700

    merge shards

--- src/indexbased/IndexMerger.java
+++ src/indexbased/IndexMerger.java
@@ -2,6 +2,7 @@ package indexbased;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -25,6 +26,8 @@ import utility.Util;
 public class IndexMerger {
     private List<FSDirectory> invertedIndexDirectories;
     private List<FSDirectory> forwardIndexDirectories;
+    public static String SHARD_STRING = "shards";
+    public static File[] nodeDirs;
 
     public IndexMerger() {
         super();
@@ -32,37 +35,43 @@ public class IndexMerger {
         this.forwardIndexDirectories = new ArrayList<FSDirectory>();
     }
 
-    private void populateIndeXdirs(String inputFile) {
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new InputStreamReader(new FileInputStream(
-                    inputFile), "UTF-8"));
-            String line;
-            int count =0;
-            while ((line = br.readLine()) != null && line.trim().length() > 0) {
-                System.out.println(count + ", "+line);
-                count++;
-                String invertedIndexDirPath = line+"/index";
-                String forwardIndexDirPath = line+"/fwd/index";
-                FSDirectory idir = FSDirectory.open(new File(invertedIndexDirPath));
-                this.invertedIndexDirectories.add(idir);
-                FSDirectory fdir = FSDirectory.open(new File(forwardIndexDirPath));
-                this.forwardIndexDirectories.add(fdir);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+    private void populateIndexdirs(int shardId) {
+        this.invertedIndexDirectories = new ArrayList<FSDirectory>();
+        this.forwardIndexDirectories = new ArrayList<FSDirectory>();
+        for (File node : IndexMerger.nodeDirs) {
+            String invertedIndexDirPath = node.getAbsolutePath()
+                    + "/index/shards/" + shardId;
+            String forwardIndexDirPath = node.getAbsolutePath()
+                    + "/fwdindex/shards/" + shardId;
+            File invertedIndexFile = new File(invertedIndexDirPath);
+            File forwardIndexFile = new File(forwardIndexDirPath);
+            if (invertedIndexFile.exists() && forwardIndexFile.exists()) {
+                FSDirectory idir;
+                FSDirectory fdir;
+                try {
+                    idir = FSDirectory.open(invertedIndexFile);
+                    fdir = FSDirectory.open(forwardIndexFile);
+                    this.invertedIndexDirectories.add(idir);
+                    this.forwardIndexDirectories.add(fdir);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                if (!invertedIndexFile.exists()) {
+                    System.out
+                            .println("inverted index for shard doesn't exist: "
+                                    + invertedIndexDirPath);
+                }
+                if (!forwardIndexFile.exists()) {
+                    System.out
+                            .println("inverted index for shard doesn't exist: "
+                                    + forwardIndexDirPath);
+                }
             }
         }
     }
 
-    private void mergeindexes() {
+    private void mergeindexes(int shardId) {
         // TODO Auto-generated method stub
         System.out.println("mering inverted indexes");
         WhitespaceAnalyzer whitespaceAnalyzer = new WhitespaceAnalyzer(
@@ -74,10 +83,10 @@ public class IndexMerger {
                 .getMergePolicy();
 
         mergePolicy.setNoCFSRatio(0);// what was this for?
-        mergePolicy.setMaxCFSSegmentSizeMB(0); 
+        mergePolicy.setMaxCFSSegmentSizeMB(0);
         IndexWriter indexWriter = null;
         try {
-            FSDirectory dir = FSDirectory.open(new File(Util.INDEX_DIR));
+            FSDirectory dir = FSDirectory.open(new File(Util.INDEX_DIR+"/"+shardId));
             indexWriter = new IndexWriter(dir, indexWriterConfig);
             FSDirectory[] dirs = this.invertedIndexDirectories
                     .toArray(new FSDirectory[this.invertedIndexDirectories
@@ -102,11 +111,11 @@ public class IndexMerger {
                 .getMergePolicy();
 
         fwdmergePolicy.setNoCFSRatio(0);// what was this for?
-        fwdmergePolicy.setMaxCFSSegmentSizeMB(0); 
+        fwdmergePolicy.setMaxCFSSegmentSizeMB(0);
         indexWriter = null;
         try {
 
-            FSDirectory dir = FSDirectory.open(new File(Util.FWD_INDEX_DIR));
+            FSDirectory dir = FSDirectory.open(new File(Util.FWD_INDEX_DIR+"/"+shardId));
             indexWriter = new IndexWriter(dir, fwdIndexWriterConfig);
             FSDirectory[] dirs = this.forwardIndexDirectories
                     .toArray(new FSDirectory[this.forwardIndexDirectories
@@ -124,19 +133,38 @@ public class IndexMerger {
             }
         }
     }
-    public static void main(String [] args) throws FileNotFoundException{
+
+    public static void populateNodeDirs() {
+        File currentDir = new File(System.getProperty("user.dir"));
+        IndexMerger.nodeDirs = currentDir.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                // TODO Auto-generated method stub
+                return pathname.isDirectory()
+                        && pathname.getName().startsWith("NODE");
+            }
+        });
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
         IndexMerger indexMerger = new IndexMerger();
-        
         EProperties properties = new EProperties();
         FileInputStream fis = null;
+        IndexMerger.populateNodeDirs();
         System.out.println("reading Q values from properties file");
         String propertiesPath = System.getProperty("properties.location");
         System.out.println("propertiesPath: " + propertiesPath);
         fis = new FileInputStream(propertiesPath);
         try {
             properties.load(fis);
-            String segmentString = properties.getProperty("SHARD_MAX_NUM_TOKENS");
+            String segmentString = properties
+                    .getProperty("SHARD_MAX_NUM_TOKENS");
             String[] shardSegments = segmentString.split(",");
+            for (int shardId = 1; shardId <= shardSegments.length; shardId++) {
+                indexMerger.populateIndexdirs(shardId);
+                indexMerger.mergeindexes(shardId);
+            }
+
         } catch (IOException e) {
             System.out.println("ERROR READING PROPERTIES FILE, "
                     + e.getMessage());
@@ -151,13 +179,6 @@ public class IndexMerger {
                 }
             }
         }
-        
-        
-        String inputFile = "kmaster";
-        System.out.println("populating index dirs");
-        indexMerger.populateIndeXdirs(inputFile);
-        System.out.println("merging");
-        indexMerger.mergeindexes();
-        System.out.println("done!");
+        System.out.println("all merge done!");
     }
 }
