a9f3a714eaa99cb8fc7f1c6487cd2a73223f5359
vpsaini <vaibps17@gmail.com>
2016-04-15 17:59:31 -0700

    run on all Qs

--- hpc.sh
+++ hpc.sh
@@ -2,11 +2,10 @@
 offset=0
 start_id="${1:-1}"
 stop_id="${2:-256}"
-queue_name="${3:-free64}"
 
 mv completed_queries.txt completed_queries_$(date +"%Y%m%d_%H%M%S")
 python completed.py rr > rr.log
-python completed.py gw $start_id $stop_id $queue_name
+python completed.py gw $start_id $stop_id
 ant clean cdi
 for i in $(seq $start_id $stop_id)
 #for i in `cat jmiss`
