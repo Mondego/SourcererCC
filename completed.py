31758525a56b4870a6632e507ee40c420988289b
vpsaini <vaibps17@gmail.com>
2016-04-25 19:14:42 -0700

    request 2-4 cores per job

--- completed.py
+++ completed.py
@@ -149,6 +149,7 @@ def createWorkers(start_worker_id=1, end_worker_id=256, queue_name="free64"):
     worker_template = """#!/bin/bash
 #$ -N search
 #$ -q {queue_name}
+#$ -pe openmp 2-4
 #$ -ckpt blcr
 node={node_id}
 rootPATH=`pwd`
