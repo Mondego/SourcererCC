b470de28e07e4b0ecdedf606d9b9bc8adcb8515e
vpsaini <vaibps17@gmail.com>
2016-04-26 13:39:24 -0700

    script to copy sourcerercc.properties to nodes

---
+++ copy_properties.sh
@@ -0,0 +1,6 @@
+#!/bin/bash                                                                                                                                                                                                 │······
+num_nodes="${1:-676}"                                                                                                                                                                                        │······
+for i in $(seq 1 1 $num_nodes)                                                                                                                                                                              │······
+do                                                                                                                                                                                                          │······
+  cp sourcerer-cc.properties "NODE_"$i/                                                                                                                                                                     │······
+done 
