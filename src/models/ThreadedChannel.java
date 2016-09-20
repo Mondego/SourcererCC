0a211633574234f820ea3e822b88549e2acac908
Crista Lopes <lopes@ics.uci.edu>
2016-09-19 20:41:12 -0700

    Attempt at throttling ThreadedChannel by blocking the senders when all threads are busy (wiggle room of 2 tasks in the queue)

--- src/models/ThreadedChannel.java
+++ src/models/ThreadedChannel.java
@@ -6,20 +6,39 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.RejectedExecutionException;
 
 public class ThreadedChannel<E> {
 
     private ExecutorService executor;
     private Class<Runnable> workerType;
+    private Semaphore semaphore;
 
     public ThreadedChannel(int nThreads, Class clazz) {
         this.executor = Executors.newFixedThreadPool(nThreads);
         this.workerType = clazz;
+	this.semaphore = new Semaphore(nThreads+2);
     }
 
     public void send(E e) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
-        Runnable o = this.workerType.getDeclaredConstructor(e.getClass()).newInstance(e);
-        executor.execute((Runnable)o);
+        final Runnable o = this.workerType.getDeclaredConstructor(e.getClass()).newInstance(e);
+	try {
+	    semaphore.acquire();
+	} catch (InterruptedException ex) {
+	    System.out.println("Caught interrupted exception " + ex);
+	}
+	    	
+	try {
+	    executor.execute(new Runnable() {
+		    public void run() {
+			try { o.run(); } 
+			finally { semaphore.release(); }
+		    }
+		});
+	} catch (RejectedExecutionException ex){
+	    semaphore.release();
+	}
     }
 
     public void shutdown() {
