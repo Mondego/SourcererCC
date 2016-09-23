364f02435f11584306b2a9631fcd14e61cee4fad
Crista Lopes <lopes@ics.uci.edu>
2016-09-23 15:03:43 -0700

    Fixed src/utility

---
+++ clone-detector/src/utility/TokensFileReader.java
@@ -0,0 +1,56 @@
+package utility;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+
+import models.ITokensFileProcessor;
+
+public class TokensFileReader {
+    private String nodeId;
+    private File file;
+    private ITokensFileProcessor processor;
+    private int maxTokens;
+
+    public TokensFileReader(String node_id, File f, int max_tokens, ITokensFileProcessor p) throws IOException {
+        this.nodeId = node_id;
+        this.file = f;
+        this.processor = p;
+        this.maxTokens = max_tokens;
+    }
+
+    public void read() throws FileNotFoundException, IOException, ParseException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line;
+        long lineNumber = 0;
+        char[] buf = new char[40];
+        while (br.read(buf, 0, 40) != -1) {
+            String prefix = new String(buf);
+            String[] parts = prefix.split(",");
+            int ntokens = Integer.parseInt(parts[2]);
+
+            if (ntokens > this.maxTokens) {
+                System.out.println(
+                        this.nodeId + " RL, file " + parts[1] + ", " + ntokens + " tokens is too big. Ignoring...");
+                char c;
+                while ((c = (char) br.read()) != '\n')
+                    ;
+            } else {
+                long startTime = System.nanoTime();
+
+                if ((line = br.readLine()) != null && line.trim().length() > 0) {
+                    this.processor.processLine(prefix + line);
+                }
+
+                long estimatedTime = System.nanoTime() - startTime;
+                System.out.println(this.nodeId + " RL " + lineNumber + ", file " + parts[1] + ", " + ntokens
+                        + " tokens in " + estimatedTime / 1000 + " micros");
+            }
+            lineNumber++;
+        }
+        br.close();
+    }
+}
--- clone-detector/src/utility/Util.java
+++ clone-detector/src/utility/Util.java
@@ -3,12 +3,15 @@
  */
 package utility;
 
+import indexbased.SearchManager;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -19,18 +22,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Random;
+import java.util.Set;
+
+import models.Bag;
+import models.TokenFrequency;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
-
-import indexbased.TermSorter;
-import models.Bag;
-import models.TokenFrequency;
 
 /**
  * @author vaibhavsaini
@@ -40,8 +45,12 @@ public class Util {
     static Random rand = new Random(5);
     public static final String CSV_DELIMITER = "~";
     public static final String INDEX_DIR = "index";
+    public static final String INDEX_DIR_TEMP = "index_temp";
     public static final String GTPM_DIR = "gtpm";
+    public static final String GLOBAL_WFM_DIR = "wfm";
     public static final String FWD_INDEX_DIR = "fwdindex";
+    public static final String FWD_INDEX_DIR_TEMP = "fwdindex_temp";
+    public static final String GTPM_INDEX_DIR = "gtpmindex";
     public static final String INDEX_DIR_NO_FILTER = "index_nofilter";
 
     /**
@@ -63,8 +72,7 @@ public class Util {
      * @param isNewline
      *            whether to start from a newline or not
      */
-    public static synchronized void writeToFile(Writer pWriter,
-            final String text, final boolean isNewline) {
+    public static synchronized void writeToFile(Writer pWriter, final String text, final boolean isNewline) {
         if (isNewline) {
             try {
                 pWriter.write(text + System.lineSeparator());
@@ -78,6 +86,11 @@ public class Util {
                 e.printStackTrace();
             }
         }
+        try {
+            pWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -87,12 +100,10 @@ public class Util {
      * @throws IOException
      * @return PrintWriter
      */
-    public static Writer openFile(String filename, boolean append)
-            throws IOException {
+    public static Writer openFile(String filename, boolean append) throws IOException {
         try {
-            Writer pWriter = new BufferedWriter(new OutputStreamWriter(
-                    new FileOutputStream(filename, append), "UTF-8"),
-                    1024 * 1000 * 2);
+            Writer pWriter = new BufferedWriter(
+                    new OutputStreamWriter(new FileOutputStream(filename, append), "UTF-8"));
             return pWriter;
 
         } catch (IOException e) {
@@ -124,22 +135,20 @@ public class Util {
     public static boolean createDirs(String dirname) {
         File dir = new File(dirname);
         if (!dir.exists()) {
+            System.out.println("creating directory: " + dirname);
             return dir.mkdirs();
         } else {
             return true;
         }
     }
 
-    public static boolean isSatisfyPosFilter(int similarity, int querySize,
-            int termsSeenInQueryBlock, int candidateSize,
-            int termsSeenInCandidate, int computedThreshold) {
+    public static boolean isSatisfyPosFilter(int similarity, int querySize, int termsSeenInQueryBlock,
+            int candidateSize, int termsSeenInCandidate, int computedThreshold) {
         return computedThreshold <= similarity
-                + Math.min(querySize - termsSeenInQueryBlock, candidateSize
-                        - termsSeenInCandidate);
+                + Math.min(querySize - termsSeenInQueryBlock, candidateSize - termsSeenInCandidate);
     }
 
-    public static void writeJsonStream(String filename,
-            Map<String, Integer> gtpm) {
+    public static void writeJsonStream(String filename, Map<String, Integer> gtpm) {
         Writer writer = null;
         try {
             writer = Util.openFile(filename, false);
@@ -162,8 +171,7 @@ public class Util {
     }
 
     public static String debug_thread() {
-        return "  Thread_id: " + Thread.currentThread().getId()
-                + " Thread_name: " + Thread.currentThread().getName();
+        return "  Thread_id: " + Thread.currentThread().getId() + " Thread_name: " + Thread.currentThread().getName();
     }
 
     public static Map<String, Integer> readJsonStream(String filename) {
@@ -171,8 +179,7 @@ public class Util {
         BufferedReader br = null;
         Map<String, Integer> gtpm = new HashMap<String, Integer>();
         try {
-            br = new BufferedReader(new InputStreamReader(new FileInputStream(
-                    filename), "UTF-8"));
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(filename), "UTF-8"), 1024 * 1024 * 512);
             String line;
             while ((line = br.readLine()) != null && line.trim().length() > 0) {
                 Gson gson = new GsonBuilder().create();
@@ -199,32 +206,44 @@ public class Util {
 
     }
 
+    public static <K, V> Map<K, V> lruCache(final int maxSize) {
+        return Collections.synchronizedMap(new LinkedHashMap<K, V>(maxSize * 4 / 3, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+                return size() > maxSize;
+            }
+        });
+    }
+
+    // This cache is shared by all threads that call sortBag
+    final static Map<String, Long> cache = lruCache(500000);
+
     public static void sortBag(Bag bag) {
         List<TokenFrequency> bagAsList = new ArrayList<TokenFrequency>(bag);
+
         Collections.sort(bagAsList, new Comparator<TokenFrequency>() {
             public int compare(TokenFrequency tfFirst, TokenFrequency tfSecond) {
-                long position1 = 0;
-                long position2 = 0;
-                try {
-                    position1 = TermSorter.globalTokenPositionMap.get(tfFirst
-                            .getToken().getValue());
-                } catch (Exception e) {
-                    position1 = -1;
-                    System.out.println("Exception in sort "
-                            + tfFirst.getToken().getValue());
-                }
-                try {
-                    position2 = TermSorter.globalTokenPositionMap.get(tfSecond
-                            .getToken().getValue());
-                } catch (Exception e) {
-                    position2 = -1;
-                    System.out.println("Exception in sort "
-                            + tfSecond.getToken().getValue());
-                }
-                if (position1 - position2 != 0) {
-                    return (int) (position1 - position2);
+                Long frequency1 = 0l;
+                Long frequency2 = 0l;
+                String k1 = tfFirst.getToken().getValue();
+                String k2 = tfSecond.getToken().getValue();
+                if (cache.containsKey(k1)) {
+                    frequency1 = cache.get(k1);
                 } else {
-                    return 1;
+                    frequency1 = SearchManager.gtpmSearcher.getFrequency(k1);
+                    cache.put(k1, frequency1);
+                }
+                if (cache.containsKey(k2)) {
+                    frequency2 = cache.get(k2);
+                } else {
+                    frequency2 = SearchManager.gtpmSearcher.getFrequency(k2);
+                    cache.put(k2, frequency2);
+                }
+                int result = frequency1.compareTo(frequency2);
+                if(result==0){
+                    return k1.compareTo(k2);
+                }else{
+                    return result;
                 }
             }
         });
@@ -233,6 +252,7 @@ public class Util {
             bag.add(tf);
         }
     }
+
     /*
      * public static int getMinimumSimilarityThreshold(QueryBlock
      * queryBlock,float threshold) { return (int) Math.ceil((threshold *
@@ -240,6 +260,101 @@ public class Util {
      * int getMinimumSimilarityThreshold(Bag bag,float threshold) { return (int)
      * Math.ceil((threshold * bag.getSize())/ (SearchManager.MUL_FACTOR*10)); }
      */
+
+    public static void writeMapToFile(String filename, Map<String, Long> map) {
+        // TODO Auto-generated method stub
+        Writer writer = null;
+        try {
+            System.out.println("writing to : " + filename);
+            writer = Util.openFile(filename, false);
+            for (Entry<String, Long> entry : map.entrySet()) {
+                String text = entry.getKey() + ":" + entry.getValue();
+                Util.writeToFile(writer, text, true);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static Map<String, Long> readMapFromFile(String filename) {
+        // TODO Auto-generated method stub
+
+        BufferedReader br = null;
+        Map<String, Long> gtpm = new HashMap<String, Long>();
+        try {
+            System.out.println("here " + filename);
+            File f = new File(filename);
+            System.out.println("file is " + f);
+            FileInputStream fis = new FileInputStream(f);
+            System.out.println(fis);
+            InputStreamReader ir = new InputStreamReader(fis, "UTF-8");
+            System.out.println(ir);
+            br = new BufferedReader(ir);
+            System.out.println(br);
+            // br = new BufferedReader(new InputStreamReader(new
+            // FileInputStream(
+            // new File(filename)), "UTF-8"), 1024 * 1024 * 512);
+
+            System.out.println("hi");
+            String line;
+            while ((line = br.readLine()) != null && line.trim().length() > 0) {
+                String[] keyValPair = line.split(":");
+                gtpm.put(keyValPair[0], Long.parseLong(keyValPair[1]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return gtpm;
+
+    }
+
+    public static void populateProcessedWFMSet(String filename, Set<String> processedWFMset) {
+        BufferedReader br = null;
+        File f = new File(filename);
+        System.out.println("file is " + f);
+        FileInputStream fis;
+        try {
+            fis = new FileInputStream(f);
+            System.out.println(fis);
+            InputStreamReader ir = new InputStreamReader(fis, "UTF-8");
+            System.out.println(ir);
+            br = new BufferedReader(ir);
+            String line;
+            while ((line = br.readLine()) != null && line.trim().length() > 0) {
+                processedWFMset.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static BufferedReader getReader(File queryFile) throws FileNotFoundException {
+        BufferedReader br = null;
+        br = new BufferedReader(new FileReader(queryFile));
+        return br;
+    }
+
+    public static Writer openFile(File output, boolean append) throws IOException {
+        // TODO Auto-generated method stub
+        Writer pWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output, append), "UTF-8"));
+        return pWriter;
+    }
 
     /*
      * public static int getPrefixSize(QueryBlock queryBlock, float threshold) {
