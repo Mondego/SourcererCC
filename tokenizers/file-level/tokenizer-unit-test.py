4ba1936fc4f35131c61305ecc0863a16aba24e11
Crista Lopes <lopes@ics.uci.edu>
2016-09-27 18:03:12 -0700

    Tokenizer: added a few more tests related to line counts, and fixed off-by-one bugs on those counts.

--- tokenizers/file-level/tokenizer-unit-test.py
+++ tokenizers/file-level/tokenizer-unit-test.py
@@ -26,6 +26,8 @@ comment_open_tag = re.escape(config.get('Language', 'comment_open_tag'))
 comment_close_tag = re.escape(config.get('Language', 'comment_close_tag'))
 comment_open_close_pattern = comment_open_tag + '.*?' + comment_close_tag
 
+REGEX = re.compile('\w+@@::@@+\d')
+
 class TestParser(unittest.TestCase):
 
     #Input is something like: @#@print@@::@@1,include@@::@@1,sys@@::@@1
@@ -34,9 +36,44 @@ class TestParser(unittest.TestCase):
 
         if len(list_tokens_string) > 3:
             split = list_tokens_string[3:].split(',')
-            REGEX = re.compile('\w+@@::@@+\d')
             for pair in split:
                 self.assertTrue(REGEX.match(pair))
+
+    def test_line_counts_1(self):
+        input = """ line 1
+                    line 2
+                    line 3 """
+        (final_stats, final_tokens, file_times) = tokenizer.tokenize(input, comment_inline_pattern, comment_open_close_pattern, separators)
+        (file_hash,lines,LOC,SLOC) = final_stats
+
+        self.assertEqual(lines,3)
+        self.assertEqual(LOC,3)
+        self.assertEqual(SLOC,3)
+
+    def test_line_counts_2(self):
+        input = """ line 1
+                    line 2
+                    line 3
+"""
+        (final_stats, final_tokens, file_times) = tokenizer.tokenize(input, comment_inline_pattern, comment_open_close_pattern, separators)
+        (file_hash,lines,LOC,SLOC) = final_stats
+
+        self.assertEqual(lines,3)
+        self.assertEqual(LOC,3)
+        self.assertEqual(SLOC,3)
+
+    def test_line_counts_3(self):
+        input = """ line 1
+
+                    // line 2
+                    line 3 
+                """
+        (final_stats, final_tokens, file_times) = tokenizer.tokenize(input, comment_inline_pattern, comment_open_close_pattern, separators)
+        (file_hash,lines,LOC,SLOC) = final_stats
+
+        self.assertEqual(lines,5)
+        self.assertEqual(LOC,3)
+        self.assertEqual(SLOC,2)
 
     def test_comments(self):
         input = "// Hello\n // World"
@@ -45,8 +82,8 @@ class TestParser(unittest.TestCase):
         (tokens_count_total,tokens_count_unique,token_hash,tokens) = final_tokens
 
 
-        self.assertEqual(lines,1)
-        self.assertEqual(LOC,1)
+        self.assertEqual(lines,2)
+        self.assertEqual(LOC,2)
         self.assertEqual(SLOC,0)
 
         self.assertEqual(tokens_count_total,0)
@@ -60,8 +97,8 @@ class TestParser(unittest.TestCase):
         (tokens_count_total,tokens_count_unique,token_hash,tokens) = final_tokens
 
 
-        self.assertEqual(lines,1)
-        self.assertEqual(LOC,1)
+        self.assertEqual(lines,2)
+        self.assertEqual(LOC,2)
         self.assertEqual(SLOC,0)
 
         self.assertEqual(tokens_count_total,0)
@@ -84,9 +121,9 @@ class TestParser(unittest.TestCase):
         (file_hash,lines,LOC,SLOC) = final_stats
         (tokens_count_total,tokens_count_unique,token_hash,tokens) = final_tokens
 
-        self.assertEqual(lines,10)
-        self.assertEqual(LOC,9)
-        self.assertEqual(SLOC,7)
+        self.assertEqual(lines,11)
+        self.assertEqual(LOC,10)
+        self.assertEqual(SLOC,8)
 
         self.assertEqual(tokens_count_total,24)
         self.assertEqual(tokens_count_unique,18)
--- tokenizers/file-level/tokenizer.py
+++ tokenizers/file-level/tokenizer.py
@@ -90,9 +90,13 @@ def tokenize(file_string, comment_inline_pattern, comment_open_close_pattern, se
     hash_time = (dt.datetime.now() - h_time).microseconds
     
     lines = file_string.count('\n')
+    if not file_string.endswith('\n'):
+        lines += 1
     file_string = "".join([s for s in file_string.splitlines(True) if s.strip()])
 
     LOC = file_string.count('\n')
+    if not file_string.endswith('\n'):
+        LOC += 1
 
     re_time = dt.datetime.now()
     # Remove tagged comments
@@ -101,9 +105,11 @@ def tokenize(file_string, comment_inline_pattern, comment_open_close_pattern, se
     file_string = re.sub(comment_inline_pattern, '', file_string, flags=re.MULTILINE)
     re_time = (dt.datetime.now() - re_time).microseconds
 
-    file_string = "".join([s for s in file_string.splitlines(True) if s.strip()])
+    file_string = "".join([s for s in file_string.splitlines(True) if s.strip()]).strip()
 
     SLOC = file_string.count('\n')
+    if file_string != '' and not file_string.endswith('\n'):
+        SLOC += 1
 
     final_stats = (file_hash,lines,LOC,SLOC)
 
