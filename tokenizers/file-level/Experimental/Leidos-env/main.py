f5ceae8dcb124f172419d8a992657c5873961bf6
Pedro Martins <pribeiro@uci.edu>
2016-12-01 17:59:09 -0800

    Main python file is already calling CC. TODO: make tokenizer merge output files and move them to CC, change from tgz reading to zip

---
+++ tokenizers/file-level/Experimental/Leidos-env/main.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import logging
+import datetime
+import MySQLdb
+import time
+import ConfigParser
+
+sys.path.append(os.path.abspath("tokenizer"))
+from tokenizerController import TokenizerController
+
+sys.path.append(os.path.abspath("../../../../clone-detector"))
+from controller import ScriptController
+
+if __name__ == '__main__':
+    print 'input',sys.argv[1]
+    tokenizerController = TokenizerController(sys.argv[1])
+    
+    params = {"num_nodes_search": 2}
+    controller = ScriptController(params)
