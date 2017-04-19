e1a9fa53744fb9a627b064448d832c3d4126a4b7
Farima Farahani <Farima Farahani>
2017-04-18 17:54:56 -0700

    Added Mod, Character Count, Skewness, Kurtosis

--- src/preprocessing/Preprocessor.java
+++ src/preprocessing/Preprocessor.java
@@ -4,58 +4,94 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Collectors;
+
 import org.apache.commons.math3.stat.StatUtils;
+import org.apache.commons.math3.stat.descriptive.moment.Kurtosis;
+import org.apache.commons.math3.stat.descriptive.moment.Skewness;
+import org.apache.commons.math3.stat.interval.IntervalUtils;
+
 /**
  * Created by Farima 
  */
 public class Preprocessor {
     public static void main(String[] args) {
+		long beginTime=System.currentTimeMillis();
         addStatsAndGiveIdsToTokens();
+		long endTime=System.currentTimeMillis();
+
     }
     private static void addStatsAndGiveIdsToTokens(){
-        int randomNumber=0;
+                int randomNumber=0;
         try {
             String eachFile="";
-            BufferedReader bf=new BufferedReader(new FileReader("blocks.file"));
-            PrintWriter writer=new PrintWriter("outputTest.txt");
-            HashMap<String,Integer> wordNumbers=new HashMap<>();
-            while ((eachFile=bf.readLine())!=null){
-                String[] separatedLineFromMeta=eachFile.split("@#@");
-                String[] words=separatedLineFromMeta[1].split(",");
-                String word="";
-                String lineNewFile="";
-                double mean;
-                double variance;
-                double mad;
-                double stdDev;
-                double median;
-                ArrayList<Double> numbersInLine=new ArrayList<>();
-                for (int i = 0; i <words.length ; i++) {
-                    word=words[i].split("@@::@@")[0];
-                    Integer frequency=Integer.valueOf(words[i].split("@@::@@")[1]);
-                    if (!wordNumbers.containsKey(word)){
-                        randomNumber++;
-                        wordNumbers.put(word,randomNumber);
-                    }
-                    for (int j = 0; j <frequency ; j++) {
-                        numbersInLine.add(Double.valueOf(wordNumbers.get(word)));
-                    }
 
-                    lineNewFile+=words[i]+"@@::@@"+wordNumbers.get(word)+",";
+            BufferedReader bf=new BufferedReader(new FileReader("input_preprocessing/farima_1k.tokens"));
+            PrintWriter writer=new PrintWriter("output_preprocessing/output_farima_1k.txt");
+            HashMap<String,Integer> wordNumbers=new HashMap<>();
+            int charactersNum=0;
+            while ((eachFile=bf.readLine())!=null) {
+                String[] separatedLineFromMeta = eachFile.split("@#@");
+                if (Integer.parseInt(separatedLineFromMeta[0].split(",")[2]) >=65&&Integer.parseInt(separatedLineFromMeta[0].split(",")[2]) <=5000) {//we do calculations just for files have more than 65 tokens and less than 5000 tokens
+                    String[] words = null;
+                    try {
+                        words = separatedLineFromMeta[1].split(",");
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        e.printStackTrace();
+                    }
+                    String word = "";
+                    String lineNewFile = "";
+                    double mean;
+                    double variance;
+                    double mad;
+                    double stdDev;
+                    double median;
+                    double mode;
+                    double skewness;
+                    double kurtosis;
+                    ArrayList<Double> numbersInLine = new ArrayList<>();
+                    for (int i = 0; i < words.length; i++) {
+                        word = words[i].split("@@::@@")[0];
+
+                        Integer frequency = Integer.valueOf(words[i].split("@@::@@")[1]);
+                        if (!wordNumbers.containsKey(word)) {
+                            randomNumber++;
+                            wordNumbers.put(word, randomNumber);
+                        }
+                        for (int j = 0; j < frequency; j++) {
+                            numbersInLine.add(Double.valueOf(wordNumbers.get(word)));
+                            charactersNum += word.length();
+                        }
+
+                        lineNewFile += words[i] + "@@::@@" + wordNumbers.get(word) + ",";
+                    }
+                    double[] numbers = new double[numbersInLine.size()];
+                    for (int i = 0; i < numbers.length; i++) {
+                        numbers[i] = numbersInLine.get(i);
+                    }
+                    Skewness sk=new Skewness();
+                    Kurtosis ku=new Kurtosis();
+                    skewness=sk.evaluate(numbers,0,numbers.length);
+                    kurtosis=ku.evaluate(numbers,0,numbers.length);
+                    median = getMedian(numbers);
+                    variance = StatUtils.variance(numbers);
+                    stdDev = Math.sqrt(variance);
+                    mean = StatUtils.mean(numbers);
+                    mad = getMad(numbers);
+                    List<Map.Entry<Double, Integer>> sortedModes =
+                            (getMode(numbers)).entrySet().stream().sorted(Map.Entry.<Double, Integer>comparingByValue().reversed()).limit(5).collect(Collectors.toList());
+//                if (separatedLineFromMeta[0].startsWith("19,915"))
+//                    System.out.println("d");
+                    writer.append(separatedLineFromMeta[0] + "," + charactersNum + "," + median + "," + mean + "," + variance + "," + stdDev
+                            + "," + mad + ","+skewness+","+kurtosis+","
+                            + (sortedModes.size() > 0 ? sortedModes.get(0).getKey() + "#" + sortedModes.get(0).getValue() : "-1#-1") + ","
+                            + (sortedModes.size() > 1 ? sortedModes.get(1).getKey() + "#" + sortedModes.get(1).getValue() : "-1#-1") + ","
+                            + (sortedModes.size() > 2 ? sortedModes.get(2).getKey() + "#" + sortedModes.get(2).getValue() : "-1#-1") + ","
+                            + (sortedModes.size() > 3 ? sortedModes.get(3).getKey() + "#" + sortedModes.get(3).getValue() : "-1#-1") + ","
+                            + (sortedModes.size() > 4 ? sortedModes.get(4).getKey() + "#" + sortedModes.get(4).getValue() : "-1#-1") + "@#@"
+                            + lineNewFile.substring(0, lineNewFile.length() - 1) + System.lineSeparator());
                 }
-                double[] numbers=new double[numbersInLine.size()];
-                for (int i = 0; i <numbers.length ; i++) {
-                    numbers[i]=numbersInLine.get(i);
-                }
-                median=getMedian(numbers);
-                variance=StatUtils.variance(numbers);
-                stdDev=Math.sqrt(variance);
-                mean=StatUtils.mean(numbers);
-                mad=getMad(numbers);
-                writer.append(separatedLineFromMeta[0]+","+median+","+mean+","+variance+","+stdDev+","+mad+"@#@"+lineNewFile.substring(0,lineNewFile.length()-1)+System.lineSeparator());
             }
             writer.close();
             bf.close();
@@ -86,6 +122,35 @@ public class Preprocessor {
             medDiffenences[i]=Math.abs(numArray[i]-mean);
         }
         return StatUtils.mean(medDiffenences);
+    }
+	
+	private static HashMap<Double,Integer> getMode(double[] numarray){
+        HashMap<Double,Integer> frequencies=new HashMap<>();
+        HashMap<Double,Integer> modes=new HashMap<>();
+        for (int i = 0; i <numarray.length ; i++) {
+            if (frequencies.containsKey(numarray[i])){
+                frequencies.put(numarray[i],frequencies.get(numarray[i])+1);
+            }
+            else {
+                frequencies.put(numarray[i],1);
+            }
+        }
+
+        Integer[] maxFreqs=new Integer[frequencies.keySet().size()];
+        int i=0;
+        for (Double number:frequencies.keySet()){
+            maxFreqs[i]=frequencies.get(number);
+            i++;
+        }
+        Arrays.sort(maxFreqs, Collections.reverseOrder()) ;
+        for (Double number:frequencies.keySet()){
+            if (frequencies.get(number)==maxFreqs[0]) modes.put(number,maxFreqs[0]);
+            else if (frequencies.get(number)==maxFreqs[1]) modes.put(number,maxFreqs[1]);
+            else if (frequencies.get(number)==maxFreqs[2]) modes.put(number,maxFreqs[2]);
+            else if (frequencies.get(number)==maxFreqs[3]) modes.put(number,maxFreqs[3]);
+            else if (frequencies.get(number)==maxFreqs[4]) modes.put(number,maxFreqs[4]);
+        }
+        return modes;
     }
 
 }
