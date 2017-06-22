3690c06479ab05128649181675c4c22a2f711227
pedromartins4 <pribeiro@uci.edu>
2017-06-22 11:05:46 -0700

    Code improvement for OOPSLA artifact

--- scripts-data-analysis/pre-CC/step4/find-distinct-token-hashes.py
+++ scripts-data-analysis/pre-CC/step4/find-distinct-token-hashes.py
@@ -1,5 +1,5 @@
 # Creates a group of files whose token-hash is representative of a group
-# Usage $pythoon this-script.py distinct-token-hash-tokens-list.tokens distinct-file-hash-stats-list.stats
+# Usage $pythoon this-script.py token-hash-folder file-hash-folder
 
 import sys
 import os
@@ -15,18 +15,25 @@ else:
 set_ids    = set()
 set_hashes = set()
 
-print 'Searching on ',sys.argv[1]
-with open(sys.argv[1],'r') as file_book, open(os.path.join(PATH_distinct_token_hashes,'distinct-tokens.tokens'),'a+') as result_tokens:
-  for line in file_book:
-    file_hash = (line.split('@#@')[0]).split(',')[4]
-    if file_hash not in set_hashes:
-      result_tokens.write(line)
-      set_hashes.add(file_hash)
-      set_ids.add(line.split(',')[1])
+for file in os.listdir(sys.argv[1]):
+  if file.endswith(".tokens"):
+    file_path = os.path.join(sys.argv[1], file)
+    print 'Searching on ',file_path
+    with open(file_path,'r') as file_book, open(os.path.join(PATH_distinct_token_hashes,'distinct-tokens.tokens'),'a+') as result_tokens:
+      for line in file_book:
+        file_hash = (line.split('@#@')[0]).split(',')[4]
+        if file_hash not in set_hashes:
+          result_tokens.write(line)
+          set_hashes.add(file_hash)
+          set_ids.add(line.split(',')[1])
 
-print 'Searching on ',sys.argv[1]
-with open(sys.argv[2],'r') as file_book, open(os.path.join(PATH_distinct_token_hashes,'distinct-tokens.stats'),'a+') as result_stats:
-  for line in file_book:
-    file_id = line.split(',')[1]
-    if file_id in set_ids:
-      result_stats.write(line)
+
+for file in os.listdir(sys.argv[2]):
+  if file.endswith(".stats"):
+    file_path = os.path.join(sys.argv[2], file)
+    print 'Searching on ',file_path
+    with open(file_path,'r') as file_book, open(os.path.join(PATH_distinct_token_hashes,'distinct-tokens.stats'),'a+') as result_stats:
+      for line in file_book:
+        file_id = line.split(',')[1]
+        if file_id in set_ids:
+          result_stats.write(line)
