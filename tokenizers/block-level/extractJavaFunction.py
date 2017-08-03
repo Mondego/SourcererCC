d15f0adbf705926691ad9fd5bd55a1ae96fc221c
pedromartins4 <pribeiro@uci.edu>
2017-08-03 16:34:18 -0700

    New block-level tokenizer with correct fqns

--- tokenizers/block-level/extractJavaFunction.py
+++ tokenizers/block-level/extractJavaFunction.py
@@ -4,12 +4,18 @@ import logging
 import traceback
 import itertools
 
+global found_parent
+
 def getFunctions(filestring, logging, file_path, separators):
   logging.info("Starting block-level parsing on " + file_path)
 
   method_string = []
   method_pos    = []
-  method_name = []
+  method_name   = []
+
+  global found_parent
+  found_parent = []
+
   tree = None
 
   try:
@@ -19,68 +25,70 @@ def getFunctions(filestring, logging, file_path, separators):
       package = 'JHawkDefaultPackage'
     else:
       package = package.name
+      print package,'####'
   except Exception as e:
     logging.warning("File " + file_path + " cannot be parsed. (1)" + str(e))
     #logging.warning('Traceback:' + traceback.print_exc())
     return (None, None, [])
 
   file_string_split = filestring.split('\n')
+  nodes = itertools.chain(tree.filter(javalang.tree.ConstructorDeclaration), tree.filter(javalang.tree.MethodDeclaration))
 
-  for path, node in tree.filter(javalang.tree.ClassDeclaration):
-    package = package+"."+node.name
-
-    inner_classes = dict()
-
-    #Methods and Class constructors
-    nodes = itertools.chain(node.filter(javalang.tree.ConstructorDeclaration), node.filter(javalang.tree.MethodDeclaration))
-
-    for path_m,node_m in nodes:
-
-      #Handling FQN's for inner classes
-      parent_name = ''
-      if isinstance(node_m,javalang.tree.MethodDeclaration):
-        parent = path_m[:-1][-1]
-        if isinstance(parent, javalang.tree.ClassCreator):
-          if parent.type.name not in inner_classes:
-            parent_name = '$'+parent.type.name
-            inner_classes[parent.type.name] = 0
-          else:
-            parent_name = '$'+parent.type.name+'_'+str(inner_classes[parent.type.name])
-            inner_classes[parent.type.name] = inner_classes[parent.type.name]+1
-
-      (init_line,b) = node_m.position
-      method_body = []
-      closed = 0
-      openned = 0
-
-      args = []
-      for t in node_m.parameters:
-        dims = []
-        if len(t.type.dimensions) > 0:
-          for e in t.type.dimensions:
-            dims.append("[]")
-        dims = "".join(dims)
-        args.append(t.type.name+dims)
-      args = ",".join(args)
-
-      for line in file_string_split[init_line-1:]:
-        closed  += line.count('}')
-        openned += line.count('{')
-        if (closed - openned) == 0:
-          method_body.append(line)
-          break
+  for path, node in nodes:
+    #print '---------------------------------------'
+    name = '.'+node.name
+    for i, var in enumerate(reversed(path)):
+      #print var, i, len(path)-3
+      if isinstance(var, javalang.tree.ClassDeclaration):
+        #print 'One Up:',var,var.name
+        if len(path)-3 == i: #Top most
+          name = '.'+var.name+check_repetition(var,var.name)+name
         else:
-          method_body.append(line)
+          name = '$'+var.name+check_repetition(var,var.name)+name
+      if isinstance(var, javalang.tree.ClassCreator):
+        #print 'One Up:',var,var.type.name
+        name = '$'+var.type.name+check_repetition(var,var.type.name)+name
+      if isinstance(var, javalang.tree.InterfaceDeclaration):
+        #print 'One Up:',var,var.name
+        name = '$'+var.name+check_repetition(var,var.name)+name
+    #print i,var,len(path)
+    #print path
+    #while len(path) != 0:
+    #  print path[:-1][-1]
+    args = []
+    for t in node.parameters:
+      dims = []
+      if len(t.type.dimensions) > 0:
+        for e in t.type.dimensions:
+          dims.append("[]")
+      dims = "".join(dims)
+      args.append(t.type.name+dims)
+    args = ",".join(args)
 
-      end_line = init_line + len(method_body) - 1
-      method_body = '\n'.join(method_body)
+    fqn = ("%s%s(%s)") % (package,name,args)
+    print "->",fqn
 
-      method_pos.append((init_line,end_line))
-      method_string.append(method_body)
+    (init_line,b) = node.position
+    method_body = []
+    closed = 0
+    openned = 0
 
-      method_name.append(("%s%s.%s(%s)") % (package,parent_name,node_m.name,args))
+    for line in file_string_split[init_line-1:]:
+      closed  += line.count('}')
+      openned += line.count('{')
+      if (closed - openned) == 0:
+        method_body.append(line)
+        break
+      else:
+        method_body.append(line)
 
-      print ("%s%s.%s(%s)") % (package,parent_name,node_m.name,args)
+    end_line = init_line + len(method_body) - 1
+    method_body = '\n'.join(method_body)
+
+    method_pos.append((init_line,end_line))
+    method_string.append(method_body)
+
+    method_name.append(fqn)
 
   if (len(method_pos) != len(method_string)):
     logging.warning("File " + file_path + " cannot be parsed. (3)")
@@ -89,6 +97,24 @@ def getFunctions(filestring, logging, file_path, separators):
     logging.warning("File " + file_path + " successfully parsed.")
     return (method_pos,method_string,method_name)
 
+def check_repetition(node,name):
+  before = -1
+  i = 0
+  for (obj,n,value) in found_parent:
+    if obj is node:
+      if value == -1:
+        return ''
+      else:
+        return '_'+str(value)
+    else:
+      i += 1
+    if n == name:
+      before += 1
+  found_parent.append((node,name,before))
+  if before == -1:
+    return ''
+  else:
+    return '_'+str(before)
 
 if __name__ == "__main__":
   # Read straight from a file, for testing purposes
