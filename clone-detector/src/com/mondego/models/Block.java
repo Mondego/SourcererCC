70ef8d226726fb9baef87e5d8c08e286734ff884
saini <vaibps17@gmail.com>
2017-08-10 13:29:57 -0700

    debug

--- clone-detector/src/com/mondego/models/Block.java
+++ clone-detector/src/com/mondego/models/Block.java
@@ -4,10 +4,10 @@
 package com.mondego.models;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.mondego.indexbased.SearchManager;
 import com.mondego.utility.BlockInfo;
@@ -31,7 +31,7 @@ public class Block {
     public List<Double> metrics;
     public String fqmn;
     public long rowId;
-
+    private static final Logger logger = LogManager.getLogger(Block.class);
     /**
      * @param id
      * @param size
@@ -44,28 +44,33 @@ public class Block {
     public void populateMetrics(String rawQuery){
     	// 465632~~selected_2351875.org.lnicholls.galleon.togo.ToGo.clean(String)~~selected~~2351875.java~~750~~763~~40~~6~~13~~10009103025115~~1~~0~~13~~109~~24~~6173.52~~0.17~~1~~1~~0~~14~~0~~1~~1~~12.35~~0~~0~~0~0~~0~~499.76~~60~~23~~49~~0~~22~~0
     	
-    	
-    	String[] columns = rawQuery.split("~~");
-    	this.rowId = Long.parseLong(columns[0]);
-    	this.fqmn = columns[1];
-    	this.projectName= columns[2];
-    	this.fileName = columns[3];
-    	this.startLine = Integer.parseInt(columns[4]);
-    	this.endLine = Integer.parseInt(columns[5]);
-    	this.size = Integer.parseInt(columns[6]);
-    	this.numUniqueTokens = Integer.parseInt(columns[7]);
-    	this.functionId = Integer.parseInt(columns[8]);
-    	this.id = Long.parseLong(columns[9]);
-    	this.metrics = new ArrayList<Double>();
-    	for (int i=10;i<columns.length;i++){
-    		this.metrics.add(Double.parseDouble(columns[i]));
+    	try{
+    		String[] columns = rawQuery.split("~~");
+        	this.rowId = Long.parseLong(columns[0]);
+        	this.fqmn = columns[1];
+        	this.projectName= columns[2];
+        	this.fileName = columns[3];
+        	this.startLine = Integer.parseInt(columns[4]);
+        	this.endLine = Integer.parseInt(columns[5]);
+        	this.size = Integer.parseInt(columns[6]);
+        	this.numUniqueTokens = Integer.parseInt(columns[7]);
+        	this.functionId = Integer.parseInt(columns[8]);
+        	this.id = Long.parseLong(columns[9]);
+        	this.metrics = new ArrayList<Double>();
+        	for (int i=10;i<columns.length;i++){
+        		this.metrics.add(Double.parseDouble(columns[i]));
+        	}
+        	this.computedThreshold = BlockInfo
+                    .getMinimumSimilarityThreshold(this.numUniqueTokens, SearchManager.th);
+            this.setMaxCandidateSize(BlockInfo
+                    .getMaximumSimilarityThreshold(this.numUniqueTokens, SearchManager.th));
+            this.prefixSize = BlockInfo.getPrefixSize(this.numUniqueTokens,
+                    this.computedThreshold);
+    	}catch (ArrayIndexOutOfBoundsException e){
+    		
+    		logger.error(e.getMessage()+", "+rawQuery);
     	}
-    	this.computedThreshold = BlockInfo
-                .getMinimumSimilarityThreshold(this.numUniqueTokens, SearchManager.th);
-        this.setMaxCandidateSize(BlockInfo
-                .getMaximumSimilarityThreshold(this.numUniqueTokens, SearchManager.th));
-        this.prefixSize = BlockInfo.getPrefixSize(this.numUniqueTokens,
-                this.computedThreshold);
+    	
     }
     /**
      * @return the id
