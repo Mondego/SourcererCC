e67b72d962af5254590b4764066443803a207e91
Pedro Martins <nhocamcc@gmail.com>
2017-10-17 10:48:22 -0700

    Sample metric implemented through AST visits

--- java-parser/src/uci/mondego/JavaMetricParser.java
+++ java-parser/src/uci/mondego/JavaMetricParser.java
@@ -1,18 +1,21 @@
 package uci.mondego;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseException;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import uci.mondego.metrics.VoidMetric;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Vector;
 
-import static com.github.javaparser.JavaParser.parse;
-import static uci.mondego.DirExplorer.finder;
-
 public class JavaMetricParser {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Vector<File> files = finder("java_samples");
+        Vector<File> files = DirExplorer.finder("java_samples");
 
         for (File f : files) {
             metricalize(f);
@@ -22,16 +25,10 @@ public class JavaMetricParser {
     public static void metricalize(File file) throws FileNotFoundException {
         System.out.println("Metricalizing " + file.getName());
 
-        CompilationUnit cUnit = parse(file);
+        CompilationUnit cUnit = JavaParser.parse(file);
 
-        //for (MethodDeclaration method : cUnit.getMethods()) {
-        // Make the visitor go through everything inside the method.
-        // method.accept(new MethodCallVisitor(), null);
-        //	System.out.println("Inside");
-        //}
-
-
-        System.out.println("   with the imports: " + cUnit.getImports());
+        // Run one metric
+        VoidMetric.run(cUnit);
     }
 
 }
