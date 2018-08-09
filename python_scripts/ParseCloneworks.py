42647b0bcfba6915e18616fdb75926a02ca8d702
Farima <farima_farahani@yahoo.com>
2018-08-09 15:41:31 -0700

    remove one extra line in parsing cw

--- python_scripts/ParseCloneworks.py
+++ python_scripts/ParseCloneworks.py
@@ -1,6 +1,6 @@
-cloneworks_clones_file_path='/lv_scratch/scratch/mondego/local/farima/new_oreo/toolsEval/CloneWorks-0.3/bcb_reduced.clones.csv'
+cloneworks_clones_file_path='/lv_scratch/scratch/mondego/local/CloneWorks-0.3/recall_dataset_conservative_th_7_mit1.clones.csv'
 # nicad_clones_file_path='D:\\PhD\\Clone\\nicad_results.txt'
-clonepairs_output_path='/lv_scratch/scratch/mondego/local/farima/new_oreo/toolsEval/CloneWorks-0.3/cloneworks_clonepairs_bceformatted.txt'
+clonepairs_output_path='/lv_scratch/scratch/mondego/local/CloneWorks-0.3/cwpais_conservative_th_7_mit1_bceformatted.txt'
 # clonepairs_output_path='..\\nicad0.3_bce_formatted.txt'
 file_output=open(clonepairs_output_path,'w')
 
@@ -15,5 +15,5 @@ with open(cloneworks_clones_file_path,'r') as file_cloneworks:
         path1=path1splitted[len(path1splitted)-2]+','+path1splitted[len(path1splitted)-1]
         path2splitted = linesplitted[3].split('/')
         path2 = path2splitted[len(path2splitted) - 2] + ',' + path2splitted[len(path2splitted) - 1]
-        file_output.write(path1+','+startline1+','+endline1+','+path2+','+startline2+','+endline2+'\n')
+        file_output.write(path1+','+startline1+','+endline1+','+path2+','+startline2+','+endline2)
 file_output.close()
