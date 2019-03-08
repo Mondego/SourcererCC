1e460f5661941563ebc7265ee4271fd745d45bf7
Poletansky Viktor <poletansky@gmail.com>
2019-03-08 14:33:29 +0300

    Revert not working error logging

--- runSourcererCC-FilesMode.sh
+++ runSourcererCC-FilesMode.sh
@@ -10,10 +10,10 @@ rm -rf files_tokens
 rm -rf bookkeeping_projs
 rm -rf file_block_stats
 rm -rf logs
-{{{python tokenizer.py zip >&1 2>&3} | while read line; do printf "\e[32m[tokenizer.py zip] \e[0m$line\n"; done} 3>&1;} | while read line; do printf "\e[32m[{stderr}tokenizer.py zip] \e[0m$line\n"; done}
+python tokenizer.py zip | while read line; do printf "\e[32m[tokenizer.py zip] \e[0m$line\n"; done
 cat files_tokens/* > blocks.file
 cp blocks.file ../../clone-detector/input/dataset/
 cd ../../clone-detector
-{{{python controller.py 10 >&1 2>&3} | while read line; do printf "\e[32m[controller.py 10] \e[0m$line\n"; done} 3>&1;} | while read line; do printf "\e[32m[{stderr}controller.py 10] \e[0m$line\n"; done}
+python controller.py 10 | while read line; do printf "\e[32m[controller.py 10] \e[0m$line\n"; done
 cd ..
 cat clone-detector/NODE_*/output8.0/query_* > results.pairs
