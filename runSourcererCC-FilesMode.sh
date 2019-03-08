bfad1620983c587e4226df9be70366ad41e0a1fb
Viktor Poletansky <poletansky@gmail.com>
2019-03-08 13:11:14 +0300

    Update runSourcererCC-FilesMode.sh

--- runSourcererCC-FilesMode.sh
+++ runSourcererCC-FilesMode.sh
@@ -10,10 +10,10 @@ rm -rf files_tokens
 rm -rf bookkeeping_projs
 rm -rf file_block_stats
 rm -rf logs
-python tokenizer.py zip
+python tokenizer.py zip | while read line; do printf "\e[32m[tokenizer.py zip] \e[0m$line\n"; done
 cat files_tokens/* > blocks.file
 cp blocks.file ../../clone-detector/input/dataset/
 cd ../../clone-detector
-python controller.py 10
+python controller.py 10 | while read line; do printf "\e[32m[controller.py 10] \e[0m$line\n"; done
 cd ..
 cat clone-detector/NODE_*/output8.0/query_* > results.pairs
