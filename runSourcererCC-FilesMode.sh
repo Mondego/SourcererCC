a6b5fa4476262148a6aa71433cd5da2809839604
Poletansky Viktor <poletansky@gmail.com>
2019-03-08 19:42:36 +0300

    Fix creating directories

--- runSourcererCC-FilesMode.sh
+++ runSourcererCC-FilesMode.sh
@@ -12,6 +12,8 @@ rm -rf file_block_stats
 rm -rf logs
 python tokenizer.py zip | while read line; do printf "\e[32m[tokenizer.py zip] \e[0m$line\n"; done
 cat files_tokens/* > blocks.file
+mkdir ../../clone-detector/input
+mkdir ../../clone-detector/input/dataset
 cp blocks.file ../../clone-detector/input/dataset/
 cd ../../clone-detector
 python controller.py 1 | while read line; do printf "\e[32m[controller.py 1] \e[0m$line\n"; done
