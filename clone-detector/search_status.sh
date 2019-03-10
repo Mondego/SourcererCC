d7f26c577884cf5e9b4d9a2e78b55f9f892e0877
Poletansky Viktor <poletansky@gmail.com>
2019-03-10 18:33:19 +0300

    Prettify search_status.sh

--- clone-detector/search_status.sh
+++ clone-detector/search_status.sh
@@ -3,13 +3,11 @@
 realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
-scriptPATH=$(realpath "$0")
-rootPATH=$(dirname $scriptPATH)
 num=$(cat search_metadata.txt)
 
 for i in $(seq 1 1 $num)
 do
- p=$(cat SCC_LOGS/NODE_$i/scc.log | grep " RL " | tail -1 | cut -d" " -f8)
+ p=$(grep " RL " SCC_LOGS/NODE_$i/scc.log | tail -1 | cut -d" " -f8)
  t=$(wc -l NODE_$i/query/query* | cut -d" " -f1)
  printf "\e[32m[search_status.sh] \e[0mNODE_$i : $p processed, out of $t\n"
 done
