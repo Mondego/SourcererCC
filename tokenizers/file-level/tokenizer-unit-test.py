287ec8bb1a5d63951131d9e347bbfe8a39772597
Poletansky Viktor <poletansky@gmail.com>
2019-03-10 13:33:21 +0300

    Fix unused variable

--- tokenizers/file-level/tokenizer-unit-test.py
+++ tokenizers/file-level/tokenizer-unit-test.py
@@ -67,7 +67,7 @@ class TestParser(unittest.TestCase):
                     // line 2
                     line 3 
                 """
-        (final_stats, final_tokens, file_times) = tokenizer.tokenize_files(input, comment_inline_pattern, comment_open_close_pattern, separators)
+        (final_stats, _, _) = tokenizer.tokenize_files(input, comment_inline_pattern, comment_open_close_pattern, separators)
         (file_hash,lines,LOC,SLOC) = final_stats
 
         self.assertEqual(lines,5)
@@ -76,9 +76,9 @@ class TestParser(unittest.TestCase):
 
     def test_comments(self):
         input = "// Hello\n // World"
-        (final_stats, final_tokens, file_times) = tokenizer.tokenize_files(input, comment_inline_pattern, comment_open_close_pattern, separators)
-        (file_hash,lines,LOC,SLOC) = final_stats
-        (tokens_count_total,tokens_count_unique,token_hash,tokens) = final_tokens
+        (final_stats, final_tokens, _) = tokenizer.tokenize_files(input, comment_inline_pattern, comment_open_close_pattern, separators)
+        (_,lines,LOC,SLOC) = final_stats
+        (tokens_count_total,tokens_count_unique,_,tokens) = final_tokens
 
         self.assertEqual(lines,2)
         self.assertEqual(LOC,2)
@@ -91,7 +91,7 @@ class TestParser(unittest.TestCase):
     def test_multiline_comment(self):
         input = '/* this is a \n comment */ /* Last one */ '
         (final_stats, final_tokens, _) = tokenizer.tokenize_files(input, comment_inline_pattern, comment_open_close_pattern, separators)
-        (file_hash,lines,LOC,SLOC) = final_stats
+        (_,lines,LOC,SLOC) = final_stats
         (tokens_count_total, tokens_count_unique, _, tokens) = final_tokens
 
         self.assertEqual(lines,2)
