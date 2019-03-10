685fc620ea9476d5833f941f9d9065dc7a0ba7ac
Poletansky Viktor <poletansky@gmail.com>
2019-03-10 13:14:24 +0300

    Fix redefining built-ins

--- scripts-data-analysis/pre-CC/step3/find-unique-file-hashes.py
+++ scripts-data-analysis/pre-CC/step3/find-unique-file-hashes.py
@@ -16,11 +16,11 @@ else:
 set_ids    = set()
 dict_hashes_frequency = {}
 
-for file in os.listdir(sys.argv[1]):
-  if file.endswith('.stats'):
-    file = os.path.join(sys.argv[1],file)
-    print 'Searching on ',file
-    with open(file,'r') as file_book:
+for filename in os.listdir(sys.argv[1]):
+  if filename.endswith('.stats'):
+    filename = os.path.join(sys.argv[1],file)
+    print 'Searching on ',filename
+    with open(filename,'r') as file_book:
       csv_reader = csv.reader(file_book, delimiter=',')
       for entry in csv_reader:
         file_hash = entry[4]
@@ -29,11 +29,11 @@ for file in os.listdir(sys.argv[1]):
         else:
           dict_hashes_frequency[file_hash] += 1
 
-for file in os.listdir(sys.argv[1]):
-  if file.endswith('.stats'):
-    file = os.path.join(sys.argv[1],file)
-    print 'Searching on ',file
-    with open(file,'r') as file_book, open(os.path.join(PATH_unique_file_hashes,'unique-files.stats'),'a+') as result_stats:
+for filename in os.listdir(sys.argv[1]):
+  if filename.endswith('.stats'):
+    filename = os.path.join(sys.argv[1],filename)
+    print 'Searching on ',filename
+    with open(filename,'r') as file_book, open(os.path.join(PATH_unique_file_hashes,'unique-files.stats'),'a+') as result_stats:
       csv_reader = csv.reader(file_book, delimiter=',')
       for entry in csv_reader:
         file_hash = entry[4]
@@ -41,11 +41,11 @@ for file in os.listdir(sys.argv[1]):
           result_stats.write(','.join(entry)+'\n')
           set_ids.add(entry[1])
 
-for file in os.listdir(sys.argv[2]):
-  if file.endswith('.tokens'):
-    file = os.path.join(sys.argv[2],file)
-    print 'Searching on ',file
-    with open(file,'r') as file_book, open(os.path.join(PATH_unique_file_hashes,'unique-files.tokens'),'a+') as result_tokens:
+for filename in os.listdir(sys.argv[2]):
+  if filename.endswith('.tokens'):
+    filename = os.path.join(sys.argv[2],filename)
+    print 'Searching on ',filename
+    with open(filename,'r') as file_book, open(os.path.join(PATH_unique_file_hashes,'unique-files.tokens'),'a+') as result_tokens:
       for line in file_book:
         file_id = line.split(',')[1]
         if file_id in set_ids:
