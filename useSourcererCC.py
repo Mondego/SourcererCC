11f9027f632372ea436e19427c0c86ea6d874c42
rprtr258 <poletansky@gmail.com>
2019-03-16 17:43:18 +0300

    Update download script

--- useSourcererCC.py
+++ useSourcererCC.py
@@ -1,10 +1,17 @@
-import requests, re, os
+#!/usr/bin/python
+import requests
+import re
+import os
+import sys
+import shutil
 
 PROJECTS_DIRECTORY = "tokenizer-sample-input"
+
 
 def download_project(url):
     archive_url = "{}/archive/master.zip".format(url)
     return requests.get(archive_url).content
+
 
 def save_project(url):
     project_content = download_project(url)
@@ -14,11 +21,34 @@ def save_project(url):
     open("tokenizers/block-level/{}/{}".format(PROJECTS_DIRECTORY, filename), "wb+").write(project_content)
     return filename
 
+
+def delete_folder(pth):
+    for sub in pth.iterdir():
+        if sub.is_dir():
+            delete_folder(sub)
+        else:
+            sub.unlink()
+    pth.rmdir()
+
+
+def create_folder(folder_name):
+    folder_name = os.path.abspath(folder_name)
+    if os.path.isdir(folder_name):
+        shutil.rmtree(folder_name)
+    os.makedirs(folder_name)
+
+
+if len(sys.argv) < 2:
+    print("This script downloads projects by urls in specified file and puts them in directories for tokenizers")
+    print("Usage:")
+    print("{} <urls.txt>".format(sys.argv[0]))
+    print("Where <urls.txt> is file with project urls")
+    exit(0)
 project_list = []
-os.makedirs("tokenizers/file-level/{}/".format(PROJECTS_DIRECTORY))
-os.makedirs("tokenizers/block-level/{}/".format(PROJECTS_DIRECTORY))
-# TODO: specify urls file in args
-with open("urls.txt") as urls_file:
+create_folder("tokenizers/file-level/{}/".format(PROJECTS_DIRECTORY))
+create_folder("tokenizers/block-level/{}/".format(PROJECTS_DIRECTORY))
+urls_filename = sys.argv[1]
+with open(urls_filename) as urls_file:
     for url in urls_file:
         url = url.strip('\n')
         filename = save_project(url)
