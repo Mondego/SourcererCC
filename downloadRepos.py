152adbb2c400b06b2e517da9d50c66572f90d18a
Poletansky Viktor <poletansky@gmail.com>
2019-03-21 18:59:15 +0300

    Extract main part in downloadRepos.py

--- downloadRepos.py
+++ downloadRepos.py
@@ -43,18 +43,20 @@ if len(sys.argv) < 4:
     print("[DESTINATION_DIR] is directory where to save archives")
     print("[PROJECTS_DIR] is directory name with project archives")
     exit(0)
-urls_filename = sys.argv[1]
-destination_directory = sys.argv[2]
-projects_directory = sys.argv[3]
-full_projects_dir_path = "{}/{}".format(destination_directory, projects_directory)
-project_list = []
-create_folder(full_projects_dir_path)
-with open(urls_filename) as urls_file:
-    for url in urls_file:
-        url = url.strip('\n')
-        filename = save_project(url, full_projects_dir_path)
-        project_list.append("{}/{}".format(projects_directory, filename))
-        user, project = re.findall(r"https://github.com/(.*)/(.*)$", url)[0]
-        print("Downloaded {}/{}".format(user, project))
 
-open("{}/project-list.txt".format(destination_directory), "w").write("\n".join(project_list))
+if __name__ == "__main__":
+    urls_filename = sys.argv[1]
+    destination_directory = sys.argv[2]
+    projects_directory = sys.argv[3]
+    full_projects_dir_path = "{}/{}".format(destination_directory, projects_directory)
+    project_list = []
+    create_folder(full_projects_dir_path)
+    with open(urls_filename) as urls_file:
+        for url in urls_file:
+            url = url.strip('\n')
+            filename = save_project(url, full_projects_dir_path)
+            project_list.append("{}/{}".format(projects_directory, filename))
+            user, project = re.findall(r"https://github.com/(.*)/(.*)$", url)[0]
+            print("Downloaded {}/{}".format(user, project))
+
+    open("{}/project-list.txt".format(destination_directory), "w").write("\n".join(project_list))
