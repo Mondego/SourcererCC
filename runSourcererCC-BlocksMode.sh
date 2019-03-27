eac7a6230c7298e777736802b9f255fb96f3f6a2
rprtr258 <poletansky@gmail.com>
2019-03-27 16:31:36 +0300

    Align run scripts

--- runSourcererCC-BlocksMode.sh
+++ runSourcererCC-BlocksMode.sh
@@ -10,8 +10,7 @@ rm -rf bookkeeping_projs
 rm -rf file_block_stats
 rm -rf logs
 python tokenizer.py zipblocks | while read line; do printf "\e[32m[tokenizer.py zipblocks] \e[0m$line\n"; done
-cat blocks_tokens/* > blocks.file
-cp blocks.file ../../clone-detector/input/dataset/
+cat blocks_tokens/* > ../../clone-detector/input/dataset/blocks.file
 cd ../../clone-detector
 python controller.py 1 | while read line; do printf "\e[32m[controller.py 1] \e[0m$line\n"; done
 cd ..
--- runSourcererCC-FilesMode.sh
+++ runSourcererCC-FilesMode.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+mkdir clone-detector/input
+mkdir clone-detector/input/dataset
 cd tokenizers/file-level/
 rm -rf files_tokens
 rm -rf bookkeeping_projs
@@ -9,9 +11,7 @@ rm -rf file_block_stats
 rm -rf logs
 python tokenizer.py zip | while read line; do printf "\e[32m[tokenizer.py zip] \e[0m$line\n"; done
 cat files_tokens/* > blocks.file
-mkdir ../../clone-detector/input
-mkdir ../../clone-detector/input/dataset
-cp blocks.file ../../clone-detector/input/dataset/
+cp blocks.file ../../clone-detector/input/dataset/blocks.file
 cd ../../clone-detector
 python controller.py 1 | while read line; do printf "\e[32m[controller.py 1] \e[0m$line\n"; done
 cd ..
