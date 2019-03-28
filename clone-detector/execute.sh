c9c929a526035806bace4ed4f3bac68619bff39d
rprtr258 <poletansky@gmail.com>
2019-03-28 10:55:05 +0300

    Fix shebangs

--- clone-detector/execute.sh
+++ clone-detector/execute.sh
@@ -6,14 +6,13 @@ realpath() {
 scriptPATH=$(realpath "$0")
 rootPATH=$(dirname $scriptPATH)
 printf "\e[32m[execute.sh] \e[0m$rootPATH\n"
-rm -rf $rootPATH/NODE*
 num_nodes="${1:-2}"
 th="${2:-8}"
 queryfile="$rootPATH/input/dataset/blocks.file"
 printf "\e[32m[execute.sh] \e[0mspliting query file $queryfile into $num_nodes parts\n"
-python $rootPATH/unevensplit.py $queryfile $num_nodes
+/$rootPATH/unevensplit.py $queryfile $num_nodes
 printf "\e[32m[execute.sh] \e[0mmoving files\n"
-bash $rootPATH/preparequery.sh $num_nodes
+/$rootPATH/preparequery.sh $num_nodes
 printf "\e[32m[execute.sh] \e[0mdone!\n"
-bash $rootPATH/replacenodeprefix.sh $num_nodes
+/$rootPATH/replacenodeprefix.sh $num_nodes
 
